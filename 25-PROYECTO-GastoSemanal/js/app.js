ad5d86637491ad8620fcb57c910c70d73495413e
Chuz <jjregis.valero@gmail.com>
2020-09-07 17:27:10 -0500

    gastos actualizado

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -24,7 +24,21 @@ class Presupuesto {
 
     nuevoGasto (gasto){
         this.gastos= [...this.gastos , gasto];
-        console.log(this.gastos);
+        this.calcularRestante();
+    //    console.log(this.gastos);
+    }
+
+    calcularRestante(){
+        const gastado = this.gastos.reduce((total, gasto)=>
+          total + gasto.cantidad ,0  
+        );
+        this.restante=this.presupuesto-gastado;
+
+        //console.log(gastado );
+    }
+    elimanarGasto(id){
+        this.gastos= this.gastos.filter(gasto =>gasto.id !== id);
+        this.calcularRestante();
     }
 
 }
@@ -62,6 +76,72 @@ class UI {
         }, 3000);
 
 
+    }
+
+agregarGastoListado(gastos){
+    this.limpiarhtml(); //elimina el html previo
+
+        console.log(gastos);
+//itetra gastos 
+gastos.forEach(gasto => {
+    const {cantidad , nombre , id} = gasto;
+    // creamos li 
+    const nuevoGasto = document.createElement('li');
+    nuevoGasto.className= 'list-group-item d-flex justify-content-between align-items-center'
+   // nuevoGasto.setAttribute('data-id',id);
+    nuevoGasto.dataset.id=id;
+    //agregat html 
+    nuevoGasto.innerHTML= `${nombre} 
+    <span class="badge badge-primary badge-pill">
+    ${cantidad}
+    </span>`;
+
+    //boton borrar gasto
+    const btnBorrar = document.createElement('button');
+    btnBorrar.classList.add('btn' , 'btn-danger' , 'borrar-gasto');
+    btnBorrar.textContent='Borrar';
+    btnBorrar.onClick = ()=>{
+        elimanarGasto(id);
+    }// asi es la forma correcta  porqeu si no manda llmar la funcion . aui munetras le de click
+    nuevoGasto.appendChild(btnBorrar);
+
+    //Agregar al html
+    gastoListado.appendChild(nuevoGasto); //no borra el html previo apendchild
+
+
+});
+    }
+
+    limpiarhtml(){
+        while(gastoListado.firstChild){
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
+
+    actualizarRestante (restante){
+        document.querySelector('#restante').textContent =restante;
+        
+    }
+
+    comprobarPresupuesto(presupustoObj){
+        const {presupuesto, restante}= presupustoObj;
+        const restantediv = document.querySelector('.restante');
+        //comprobar el 25 %
+        if((presupuesto/4)>restante){
+            restantediv.classList.remove('alert-success', 'alert-warning');
+            restantediv.classList.add('alert-danger');
+        }else if ((presupuesto/2)>restante){
+            restantediv.classList.remove('alert-success');
+            restantediv.classList.add('alert-warning');
+        }else{
+            restantediv.classList.remove('alert-danger' , 'alert-warning');
+            restantediv.classList.remove('alert-success');
+        }
+            // si el total es cero o menor 
+            if(restante<=0){
+                ui.imprimirAlerta('presupuesto agotado' , 'error');
+                formulario.querySelector('button[type="submit"]').disabled=true;
+            }
     }
 
 }
@@ -102,7 +182,29 @@ function agregarGasto (e){
 
     ///objeto con el gasto 
     const gasto = {nombre , cantidad , id:Date.now}; //une nombre y cantidad  a agasto 
-    Presupuesto.nuevoGasto(gasto);
+    presupuesto.nuevoGasto(gasto);
+    ui.imprimirAlerta('Gasto agregado');
 
+//Imprimir los gatos 
+const {gastos , restante}= presupuesto;
+ui.agregarGastoListado(gastos);
 
+ui.actualizarRestante(restante);
+
+ui.comprobarPresupuesto(presupuesto);
+
+    formulario.reset();
+
+}
+
+function elimanarGasto(id){
+    //elimna del objeto
+    presupuesto.elimanarGasto(id);
+    
+   // elimna del html
+    const {gastos , presupuesto } = presupuesto
+    ui.agregarGastoListado(gastos);
+
+    ui.actualizarRestante(restante);
+    ui.comprobarPresupuesto(presupuesto);
 }
